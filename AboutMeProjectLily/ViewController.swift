0084992b457d5609059325eb3030de6b3c8908a1
Lily Windmiller <mslilywind@gmail.com>
2022-06-23 11:46:52 -0500

    Iteration 1 of the About Me project for Kode with Klossy App Development 2022

--- AboutMeProjectLily/ViewController.swift
+++ AboutMeProjectLily/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var MusicOutlet: UILabel!
+    
+    @IBAction func MusicButton(_ sender: Any) {
+        MusicOutlet.text = "My favorite artists are Phoebe Bridgers, Mitski, and Joji!"
+    }
+    
+    
+    @IBOutlet weak var movieOutlet: UILabel!
+    
+    var buttonCounter = 0
+    
+    @IBAction func movieButton(_ sender: Any) {
+        buttonCounter += 1
+        if (buttonCounter == 1){
+            movieOutlet.text = "My favorite movie is Lady Bird."
+        } else if (buttonCounter == 2){
+            movieOutlet.text = "My favorite TV show is Old Enough."
+            buttonCounter = 0
+        }
+    }
 }
 
